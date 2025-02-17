fd00dd77d5d55855e8f31aaf32c75eb7313ac031
Bastian Opatz <bopatz@brockhaus-ag.de>
2025-02-17 16:05:38 +0100

    init

---
+++ src/webparts/bagVisionWebpart/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IBagVisionWebpartWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'BagVisionWebpartWebPartStrings' {
+  const strings: IBagVisionWebpartWebPartStrings;
+  export = strings;
+}
