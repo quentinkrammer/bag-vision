5a1860f2e6aee360b0c6a67900c052878cd17b1c
Bastian Opatz <bopatz@brockhaus-ag.de>
2025-02-18 11:58:43 +0100

    test some stuff

--- src/webparts/bagVisionWebpart/BagVisionWebpartWebPart.ts
+++ src/webparts/bagVisionWebpart/BagVisionWebpartWebPart.ts
@@ -1,107 +1,71 @@
-import { Version } from '@microsoft/sp-core-library';
-import {
-  type IPropertyPaneConfiguration,
-  PropertyPaneTextField
-} from '@microsoft/sp-property-pane';
-import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
-import type { IReadonlyTheme } from '@microsoft/sp-component-base';
-import { escape } from '@microsoft/sp-lodash-subset';
+import { SPComponentLoader } from "@microsoft/sp-loader";
+import { escape } from "@microsoft/sp-lodash-subset";
+import { BaseClientSideWebPart } from "@microsoft/sp-webpart-base";
 
-import styles from './BagVisionWebpartWebPart.module.scss';
-import * as strings from 'BagVisionWebpartWebPartStrings';
+import {
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField,
+} from "@microsoft/sp-property-pane";
+import * as strings from "BagVisionWebpartWebPartStrings";
+import styles from "./BagVisionWebpartWebPart.module.scss";
 
 export interface IBagVisionWebpartWebPartProps {
   description: string;
 }
 
 export default class BagVisionWebpartWebPart extends BaseClientSideWebPart<IBagVisionWebpartWebPartProps> {
-
-  private _isDarkTheme: boolean = false;
-  private _environmentMessage: string = '';
-
   public render(): void {
     this.domElement.innerHTML = `
-    <section class="${styles.bagVisionWebpart} ${!!this.context.sdks.microsoftTeams ? styles.teams : ''}">
+    <section class="${styles.bagVisionWebpart} ">
       <div class="${styles.welcome}">
-        <img alt="" src="${this._isDarkTheme ? require('./assets/welcome-dark.png') : require('./assets/welcome-light.png')}" class="${styles.welcomeImage}" />
-        <h2>Well done, ${escape(this.context.pageContext.user.displayName)}!</h2>
-        <div>${this._environmentMessage}</div>
-        <div>Web part property value: <strong>${escape(this.properties.description)}</strong></div>
+        <img alt="" src="${require("./assets/welcome-light.png")}" class="${
+      styles.welcomeImage
+    }" />
+        <h2>Well done, ${escape(
+          this.context.pageContext.user.displayName
+        )}!</h2>
+    
+        <div>Web part property value: <strong>${escape(
+          this.properties.description
+        )}</strong></div>
       </div>
       <div>
-        <h3>Welcome to SharePoint Framework!</h3>
-        <p>
-        The SharePoint Framework (SPFx) is a extensibility model for Microsoft Viva, Microsoft Teams and SharePoint. It's the easiest way to extend Microsoft 365 with automatic Single Sign On, automatic hosting and industry standard tooling.
-        </p>
-        <h4>Learn more about SPFx development:</h4>
-          <ul class="${styles.links}">
-            <li><a href="https://aka.ms/spfx" target="_blank">SharePoint Framework Overview</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-graph" target="_blank">Use Microsoft Graph in your solution</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-teams" target="_blank">Build for Microsoft Teams using SharePoint Framework</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-viva" target="_blank">Build for Microsoft Viva Connections using SharePoint Framework</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-store" target="_blank">Publish SharePoint Framework applications to the marketplace</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-api" target="_blank">SharePoint Framework API reference</a></li>
-            <li><a href="https://aka.ms/m365pnp" target="_blank">Microsoft 365 Developer Community</a></li>
-          </ul>
+       Moin
       </div>
-    </section>`;
-  }
+    </section>
+    <button type="button">Show welcome message</button>
+    <article id="foo-anchor">
+    
+    </article>`;
 
-  protected onInit(): Promise<void> {
-    return this._getEnvironmentMessage().then(message => {
-      this._environmentMessage = message;
-    });
-  }
-
-
-
-  private _getEnvironmentMessage(): Promise<string> {
-    if (!!this.context.sdks.microsoftTeams) { // running in Teams, office.com or Outlook
-      return this.context.sdks.microsoftTeams.teamsJs.app.getContext()
-        .then(context => {
-          let environmentMessage: string = '';
-          switch (context.app.host.name) {
-            case 'Office': // running in Office
-              environmentMessage = this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentOffice : strings.AppOfficeEnvironment;
-              break;
-            case 'Outlook': // running in Outlook
-              environmentMessage = this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentOutlook : strings.AppOutlookEnvironment;
-              break;
-            case 'Teams': // running in Teams
-            case 'TeamsModern':
-              environmentMessage = this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentTeams : strings.AppTeamsTabEnvironment;
-              break;
-            default:
-              environmentMessage = strings.UnknownEnvironment;
-          }
-
-          return environmentMessage;
-        });
+    if (!this.renderedOnce) {
+      this.onButton();
+      this.fooAnchor();
+      this.loadAssets();
     }
-
-    return Promise.resolve(this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentSharePoint : strings.AppSharePointEnvironment);
   }
 
-  protected onThemeChanged(currentTheme: IReadonlyTheme | undefined): void {
-    if (!currentTheme) {
-      return;
-    }
-
-    this._isDarkTheme = !!currentTheme.isInverted;
-    const {
-      semanticColors
-    } = currentTheme;
-
-    if (semanticColors) {
-      this.domElement.style.setProperty('--bodyText', semanticColors.bodyText || null);
-      this.domElement.style.setProperty('--link', semanticColors.link || null);
-      this.domElement.style.setProperty('--linkHovered', semanticColors.linkHovered || null);
-    }
-
+  private loadAssets() {
+    SPComponentLoader.loadScript(require("./assets/script.js")).catch(() => {});
+    SPComponentLoader.loadCss(require("./assets/style.css"));
   }
 
-  protected get dataVersion(): Version {
-    return Version.parse('1.0');
+  private fooAnchor() {
+    const fooElement = document.createElement("h1");
+    fooElement.innerText = "Foo";
+    fooElement.classList.add("foo");
+
+    const fooAnchorElement = document.querySelector("#foo-anchor");
+    fooAnchorElement?.appendChild(fooElement);
+  }
+
+  private onButton() {
+    this.domElement
+      .getElementsByTagName("button")[0]
+      .addEventListener("click", (event: MouseEvent) => {
+        event.preventDefault();
+        alert("Welcome to the SharePoint Framework!");
+      });
   }
 
   protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
@@ -109,20 +73,20 @@ export default class BagVisionWebpartWebPart extends BaseClientSideWebPart<IBagV
       pages: [
         {
           header: {
-            description: strings.PropertyPaneDescription
+            description: strings.PropertyPaneDescription,
           },
           groups: [
             {
               groupName: strings.BasicGroupName,
               groupFields: [
-                PropertyPaneTextField('description', {
-                  label: strings.DescriptionFieldLabel
-                })
-              ]
-            }
-          ]
-        }
-      ]
+                PropertyPaneTextField("description", {
+                  label: strings.DescriptionFieldLabel,
+                }),
+              ],
+            },
+          ],
+        },
+      ],
     };
   }
 }
